2cf1568c4953c8e7c2e1d1df122c6a5c4f887e00
Jenish Jadav <100338684+Jenis07@users.noreply.github.com>
2024-08-06 09:22:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ disappearance_time = None
 face_present = False
 
 # Video capture from webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 if not cap.isOpened():
     print("Error: Camera not accessible")
